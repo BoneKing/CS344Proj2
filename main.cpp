9098104470c78705816803030ba3d325b19efdb8
BoneKing <mahonea@clarkson.edu>
2019-04-15 16:47:53 -0400

    Skeleton for class methods

--- main.cpp
+++ main.cpp
@@ -3,15 +3,21 @@
 #include<string>
 
 using namespace std;
-
+template<typename T>
 class HashTable{
 public:
-
+	void size(){};
+	void empty(){};
+	void find(int key){};
+	void erase(int key){};
+	void insert(int key, T element){};
+	void clear(){};
 private:
-
-}
+	int index;
+	vector<T><T> colisions;
+};
 
 int main(){
-
-    return 0;
+	HashTable H1;
+	return 0;
 }
